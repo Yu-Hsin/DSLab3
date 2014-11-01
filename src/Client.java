b24e1acb9113fe829be51ada25f4b71ef8ab2ae3
Yu-Hsin Kuo <yuhsink@cs.cmu.edu>
2014-11-01 17:00:25 -0400

    done with client

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Client {
+
+    private ObjectInputStream inputStream;
+    private ServerSocket socketclient;
+    private static int port2client;
+
+    // open a socket for connetion to the master
+    public void openSocket() {
+	try {
+	    socketclient = new ServerSocket(port2client);
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
+    }
+
+    public void downloadFile() {
+	try {
+	    Socket socket = socketclient.accept();
+	    inputStream = new ObjectInputStream(socket.getInputStream());
+	    BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+	    String str = "";
+	    while ((str = br.readLine()) != null) {
+		System.out.println(str);
+	    }
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
+    }
+
+
+    public static void main(String[] args) {
+	Client client = new Client();
+	client.openSocket();
+	client.downloadFile();
+    }
 
 }
